ce4d7bcd0682657648728ac53d783deddc18fc8b
Daniel Simpkins <dpsimpkins@gmail.com>
2025-05-01 11:07:02 -0500

    Update init.lua

--- nvim/init.lua
+++ nvim/init.lua
@@ -2,9 +2,10 @@ local Plug = vim.fn["plug#"]
 
 vim.call('plug#begin', '~/.config/nvim/plugged')
 
+Plug 'leafOfTree/vim-svelte-plugin'
 Plug 'neovim/nvim-lspconfig'
-Plug 'rust-lang/rust.vim'
-Plug('neoclide/coc.nvim', {branch = 'release'})
+Plug 'omnisharp/omnisharp-roslyn'
+Plug 'MunifTanjim/prettier.nvim'
 Plug 'kyazdani42/nvim-web-devicons'
 Plug 'kyazdani42/nvim-tree.lua'
 Plug 'nvim-lua/plenary.nvim'
@@ -12,189 +13,58 @@ Plug 'nvim-telescope/telescope.nvim'
 Plug 'nvim-lualine/lualine.nvim'
 Plug 'lukas-reineke/indent-blankline.nvim'
 Plug 'vim-autoformat/vim-autoformat'
-Plug 'nvim-treesitter/nvim-treesitter'
 Plug 'rhysd/vim-clang-format'
-Plug 'mfussenegger/nvim-dap'
-Plug 'klen/nvim-config-local'
-Plug 'shaunsingh/nord.nvim'
-Plug 'simrat39/rust-tools.nvim'
-Plug 'romgrk/barbar.nvim'
+Plug('folke/tokyonight.nvim', {branch = 'main' })
 Plug('akinsho/toggleterm.nvim', {tag = 'v2.*'})
+Plug 'ray-x/lsp_signature.nvim'
+Plug 'jose-elias-alvarez/null-ls.nvim'
+Plug 'onsails/lspkind.nvim'
+Plug 'folke/trouble.nvim'
+
+Plug 'lewis6991/gitsigns.nvim'
+Plug 'romgrk/barbar.nvim'
+
+-- Autocomplete
+Plug 'hrsh7th/cmp-nvim-lsp'
+Plug 'hrsh7th/cmp-buffer'
+Plug 'hrsh7th/cmp-path'
+Plug 'hrsh7th/cmp-cmdline'
+Plug 'hrsh7th/nvim-cmp'
 
 vim.call('plug#end')
 
 -- Styling commands
 
-vim.cmd[[colorscheme nord]]
+vim.cmd[[colorscheme tokyonight-storm]]
 
+vim.api.nvim_command('set number relativenumber')
 vim.api.nvim_command('set mouse=a')
 vim.api.nvim_command('set expandtab')
 vim.api.nvim_command('set tabstop=2')
 vim.api.nvim_command('set shiftwidth=2')
 vim.api.nvim_command('set number')
-vim.api.nvim_command('set exrc')
-vim.api.nvim_command('set secure')
 
 local opts = { noremap=true, silent=true }
 vim.keymap.set('n', '<space>e', vim.diagnostic.open_float, opts)
 vim.keymap.set('n', '[d', vim.diagnostic.goto_prev, opts)
 vim.keymap.set('n', ']d', vim.diagnostic.goto_next, opts)
 vim.keymap.set('n', '<space>q', vim.diagnostic.setloclist, opts)
+vim.keymap.set('i', 'kj', '<esc>', opts)
+vim.keymap.set('i', 'jk', '<esc>', opts)
 
--- Setup for local nvim config
-
-require'config-local'.setup {
-  config_files = { '.nvimrc.lua', '.nvimrc' },
-  hashfile = vim.fn.stdpath('data') .. '/config-local',
-  autocommands_create = true,
-  commands_create = true,
-  silent = false,
-  lookup_parents = false
-}
-
--- Setup for Barbar
-
-local map = vim.api.nvim_set_keymap
-local opts = { noremap = true, silent = true }
-
--- Move to previous/next
-map('n', '<A-,>', '<Cmd>BufferPrevious<CR>', opts)
-map('n', '<A-.>', '<Cmd>BufferNext<CR>', opts)
--- Re-order to previous/next
-map('n', '<A-<>', '<Cmd>BufferMovePrevious<CR>', opts)
-map('n', '<A->>', '<Cmd>BufferMoveNext<CR>', opts)
--- Goto buffer in position...
-map('n', '<A-1>', '<Cmd>BufferGoto 1<CR>', opts)
-map('n', '<A-2>', '<Cmd>BufferGoto 2<CR>', opts)
-map('n', '<A-3>', '<Cmd>BufferGoto 3<CR>', opts)
-map('n', '<A-4>', '<Cmd>BufferGoto 4<CR>', opts)
-map('n', '<A-5>', '<Cmd>BufferGoto 5<CR>', opts)
-map('n', '<A-6>', '<Cmd>BufferGoto 6<CR>', opts)
-map('n', '<A-7>', '<Cmd>BufferGoto 7<CR>', opts)
-map('n', '<A-8>', '<Cmd>BufferGoto 8<CR>', opts)
-map('n', '<A-9>', '<Cmd>BufferGoto 9<CR>', opts)
-map('n', '<A-0>', '<Cmd>BufferLast<CR>', opts)
--- Pin/unpin buffer
-map('n', '<A-p>', '<Cmd>BufferPin<CR>', opts)
--- Close buffer
-map('n', '<A-c>', '<Cmd>BufferClose<CR>', opts)
--- Wipeout buffer
---                 :BufferWipeout
--- Close commands
---                 :BufferCloseAllButCurrent
---                 :BufferCloseAllButPinned
---                 :BufferCloseAllButCurrentOrPinned
---                 :BufferCloseBuffersLeft
---                 :BufferCloseBuffersRight
--- Magic buffer-picking mode
-map('n', '<C-p>', '<Cmd>BufferPick<CR>', opts)
--- Sort automatically by...
-map('n', '<Space>bb', '<Cmd>BufferOrderByBufferNumber<CR>', opts)
-map('n', '<Space>bd', '<Cmd>BufferOrderByDirectory<CR>', opts)
-map('n', '<Space>bl', '<Cmd>BufferOrderByLanguage<CR>', opts)
-map('n', '<Space>bw', '<Cmd>BufferOrderByWindowNumber<CR>', opts)
-
-require('bufferline').setup{
--- Enable/disable animations
-  animation = true,
-
-  -- Enable/disable auto-hiding the tab bar when there is a single buffer
-  auto_hide = false,
-
-  -- Enable/disable current/total tabpages indicator (top right corner)
-  tabpages = true,
-
-  -- Enable/disable close button
-  closable = true,
-
-  -- Enables/disable clickable tabs
-  --  - left-click: go to buffer
-  --  - middle-click: delete buffer
-  clickable = true,
-
-  -- Excludes buffers from the tabline
-  exclude_ft = {'javascript'},
-  exclude_name = {'package.json'},
-
-  -- Enable/disable icons
-  -- if set to 'numbers', will show buffer index in the tabline
-  -- if set to 'both', will show buffer index and icons in the tabline
-  icons = true,
-
-  -- If set, the icon color will follow its corresponding buffer
-  -- highlight group. By default, the Buffer*Icon group is linked to the
-  -- Buffer* group (see Highlighting below). Otherwise, it will take its
-  -- default value as defined by devicons.
-  icon_custom_colors = false,
-
-  -- Configure icons on the bufferline.
-  icon_separator_active = '▎',
-  icon_separator_inactive = '▎',
-  icon_close_tab = '',
-  icon_close_tab_modified = '●',
-  icon_pinned = '車',
-
-  -- If true, new buffers will be inserted at the start/end of the list.
-  -- Default is to insert after current buffer.
-  insert_at_end = false,
-  insert_at_start = false,
-
-  -- Sets the maximum padding width with which to surround each tab
-  maximum_padding = 1,
-
-  -- Sets the minimum padding width with which to surround each tab
-  minimum_padding = 1,
-
-  -- Sets the maximum buffer name length.
-  maximum_length = 30,
-
-  -- If set, the letters for each buffer in buffer-pick mode will be
-  -- assigned based on their name. Otherwise or in case all letters are
-  -- already assigned, the behavior is to assign letters in order of
-  -- usability (see order below)
-  semantic_letters = true,
-
-  -- New buffer letters are assigned in this order. This order is
-  -- optimal for the qwerty keyboard layout but might need adjustement
-  -- for other layouts.
-  letters = 'asdfjkl;ghnmxcvbziowerutyqpASDFJKLGHNMXCVBZIOWERUTYQP',
-
-  -- Sets the name of unnamed buffers. By default format is "[Buffer X]"
-  -- where X is the buffer number. But only a static string is accepted here.
-  no_name_title = nil
-}
-
--- Setup for Terminal
-
-require'toggleterm'.setup{
-  open_mapping = [[<c-\>]],
-  shading_factor = '0',
-  direction = 'float',
-  float_opts = {
-    border = 'curved'
-  },
-  winbar = {
-    enabled = true
-  }
-}
-
--- Move lines
-
-vim.keymap.set('n', '<A-Down>', ':m .+1<CR>==', opts)
-vim.keymap.set('n', '<A-Up>', ':m .-2<CR>==', opts)
-vim.keymap.set('i', '<A-Down>', '<ESC>:m .+1<CR>==gi', opts)
-vim.keymap.set('i', '<A-Up>', '<ESC>:m .-2<CR>==gi', opts)
-vim.keymap.set('v', '<A-Down>', ':m \'>.+1<CR>gv==gv', opts)
-vim.keymap.set('v', '<A-Up>', ':m \'<.-2<CR>gv==gv', opts)
-
--- Setup for LSP
-
+local protocol = require('vim.lsp.protocol')
 local on_attach = function(client, bufnr)
-  -- Enable completion triggered by <c-x><c-o>
-  vim.api.nvim_buf_set_option(bufnr, 'omnifunc', 'v:lua.vim.lsp.omnifunc')
+  local function buf_set_option(...) vim.api.nvim_buf_set_option(bufnr, ...) end
+  buf_set_option('omnifunc', 'v:lua.vim.lsp.omnifunc')
 
-  -- Mappings.
-  -- See `:help vim.lsp.*` for documentation on any of the below functions
+  if client.server_capabilities.documentFormattingProvider then
+    vim.api.nvim_create_autocmd("BufWritePre", {
+      group = vim.api.nvim_create_augroup("Format", {clear = true}),
+      buffer = bufnr,
+      callback = function() vim.lsp.buf.format() end
+    })
+  end
+
   local bufopts = { noremap=true, silent=true, buffer=bufnr }
   vim.keymap.set('n', 'gD', vim.lsp.buf.declaration, bufopts)
   vim.keymap.set('n', 'gd', vim.lsp.buf.definition, bufopts)
@@ -210,104 +80,243 @@ local on_attach = function(client, bufnr)
   vim.keymap.set('n', '<space>rn', vim.lsp.buf.rename, bufopts)
   vim.keymap.set('n', '<space>ca', vim.lsp.buf.code_action, bufopts)
   vim.keymap.set('n', 'gr', vim.lsp.buf.references, bufopts)
-  vim.keymap.set('n', '<space>f', vim.lsp.buf.formatting, bufopts)
+  --vim.keymap.set('n', '<space>f', vim.lsp.buf.formatting, bufopts)
 end
 
-local lsp_flags = {
-  debounce_text_changes = 150
+local capabilities = require('cmp_nvim_lsp').default_capabilities(
+  protocol.make_client_capabilities()
+)
+
+local null_ls = require('null-ls')
+null_ls.setup({
+    sources = {
+        null_ls.builtins.formatting.prettierd,
+    },
+})
+
+require('prettier').setup {
+  bin = 'prettierd',
+  filetypes = {
+    'css',
+    'javascript',
+    'javascriptreact',
+    'typescript',
+    'typescriptreact',
+    'json',
+    'scss',
+    'less'
+  }
 }
 
-vim.cmd [[autocmd BufWritePre <buffer> lua vim.lsp.buf.formatting_sync()]]
+local lspkind = require('lspkind')
+local cmp = require('cmp')
+cmp.setup{
+  mapping = cmp.mapping.preset.insert({
+    ['<C-d>'] = cmp.mapping.scroll_docs(-4),
+    ['<C-f>'] = cmp.mapping.scroll_docs(4),
+    ['<C-Space>'] = cmp.mapping.complete(),
+    ['<C-e>'] = cmp.mapping.close(),
+    ['<CR>'] = cmp.mapping.confirm({
+      behavior = cmp.ConfirmBehavior.Replace,
+    })
+  }),
+  snippet = {
+    expand = function(args)
+      vim.snippet.expand(args.body)
+    end,
+  },
+  sources = cmp.config.sources({
+    { name = 'nvim_lsp' },
+  }, {
+    { name = 'buffer' }
+  }),
+  formatting = {
+    format = lspkind.cmp_format({ with_text = false, maxwidth = 50 })
+  },
+  window = {
+    completion = cmp.config.window.bordered(),
+    documentation = cmp.config.window.bordered(),
+  }
+}
+
+local symbols = { Error = "󰅙", Info = "󰋼", Hint = "󰌵", Warn = "" }
+for name, icon in pairs(symbols) do
+  local hl = "DiagnosticSign" .. name
+  vim.fn.sign_define(hl, { text = icon, texthl = hl, numhl = hl })
+end
+
+vim.cmd [[
+  set completeopt=menuone,noinsert,noselect
+]]
+--highlight! default link CmpItemKind CmpItemMenuDefault
+
+-- Setup for barbar
+
+local map = vim.api.nvim_set_keymap
+local opts = { noremap = true, silent = true }
+
+map('n', '<A-,>', '<Cmd>BufferPrevious<CR>', opts)
+map('n', '<A-.>', '<Cmd>BufferNext<CR>', opts)
+map('n', '<A-<>', '<Cmd>BufferMovePrevious<CR>', opts)
+map('n', '<A->>', '<Cmd>BufferMoveNext<CR>', opts)
+map('n', '<A-1>', '<Cmd>BufferGoto 1<CR>', opts)
+map('n', '<A-2>', '<Cmd>BufferGoto 2<CR>', opts)
+map('n', '<A-3>', '<Cmd>BufferGoto 3<CR>', opts)
+map('n', '<A-4>', '<Cmd>BufferGoto 4<CR>', opts)
+map('n', '<A-5>', '<Cmd>BufferGoto 5<CR>', opts)
+map('n', '<A-6>', '<Cmd>BufferGoto 6<CR>', opts)
+map('n', '<A-7>', '<Cmd>BufferGoto 7<CR>', opts)
+map('n', '<A-8>', '<Cmd>BufferGoto 8<CR>', opts)
+map('n', '<A-9>', '<Cmd>BufferGoto 9<CR>', opts)
+map('n', '<A-0>', '<Cmd>BufferLast<CR>', opts)
+map('n', '<A-p>', '<Cmd>BufferPin<CR>', opts)
+map('n', '<A-c>', '<Cmd>BufferClose<CR>', opts)
+map('n', '<C-p>', '<Cmd>BufferPick<CR>', opts)
+map('n', '<Space>bb', '<Cmd>BufferOrderByBufferNumber<CR>', opts)
+map('n', '<Space>bd', '<Cmd>BufferOrderByDirectory<CR>', opts)
+map('n', '<Space>bl', '<Cmd>BufferOrderByLanguage<CR>', opts)
+map('n', '<Space>bw', '<Cmd>BufferOrderByWindowNumber<CR>', opts)
+
+-- Setup for Terminal
+
+require("toggleterm").setup{
+  open_mapping = [[<c-\>]],
+  shade_terminals = true,
+  shading_factor = '1',
+  direction = 'float',
+  close_on_exit = true,
+  persist_size = true,
+  float_opts = {
+    border = 'curved'
+  }
+}
+
+-- Setup for LSP's
 
 local lsp = require'lspconfig'
-
---[[ lsp.eslint.setup {
+lsp['ts_ls'].setup {
   on_attach = on_attach,
-  flags = lsp_flags
-} --]]
-
-lsp["rust_analyzer"].setup {
+  filetypes = { 'typescript', "typescriptreact", 'typescript.tsx' },
+  cmd = {'typescript-language-server', '--stdio'},
+  capabilities = capabilities
+}
+lsp.svelte.setup {
   on_attach = on_attach,
-  flags = lsp_flags,
-  settings = {
-    ["rust-analyzer"] = {
-      procMacro = { enable = true },
-      diagnostic = { disabled = {"unresolved-proc-macro"} }
-    }
+  capabilities = capabilities,
+  cmd = {'svelteserver', '--stdio'}
+}
+local pid = vim.fn.getpid();
+lsp.omnisharp.setup {
+  cmd = {'/Users/daniel/.local/bin/omnisharp/run', '--languageserver', '--hostPID',  tostring(pid)},
+  capabilities = capabilities,
+  on_attach = on_attach
+}
+
+lsp.clangd.setup {
+  on_attach = on_attach,
+  capabilities = capabilities,
+  cmd = {
+    'clangd',
+    '--offset-encoding=utf-16'
   }
 }
+lsp['rust_analyzer'].setup {
+  on_attach = on_attach,
+  capabilities = capabilities
+}
 
-vim.g.rustfmt_autosave = 1
-
-require('rust-tools').setup{}
-
-
-lsp.ccls.setup {
-  init_options = {
-    compilationDatabaseDirectory = 'builddir'
+require "lsp_signature".setup({
+  bind = true, -- This is mandatory, otherwise border config won't get registered.
+  handler_opts = {
+    border = "rounded"
   }
-}
+})
 
---- Setup for DAP
-
-local dap = require('dap')
-dap.adapters.cppdbg = {
-  id = 'cppdbg',
-  type = 'executable',
-  command = '~/.local/bin/extension/debugAdapters/bin/OpenDebugAD7'
-}
-
--- Setup for NVIM Tree
+vim.api.nvim_create_autocmd('FileType', {pattern = {'c','cpp', 'h', 'hpp'}, command = 'ClangFormatAutoEnable'})
+vim.api.nvim_create_autocmd('BufWritePre', {pattern = {'tsx', 'ts', 'jsx', 'js'}, command='EslintFixAll'})
 
 require('nvim-tree').setup()
 vim.keymap.set('n', '<F5>', ':NvimTreeToggle<CR>', opts)
 
 -- Setup for Telescope
 
-require('telescope').setup()
-vim.keymap.set('n', '<F6>', ':Telescope live_grep<CR>', opts)
-vim.keymap.set('n', '<C-F>', ':Telescope current_buffer_fuzzy_find<CR>', opts)
-vim.keymap.set('i', '<F6>', '<ESC>:Telescope live_grep<CR>', opts)
-vim.keymap.set('i', '<C-F>', '<ESC>:Telescope current_buffer_fuzzy_find<CR>', opts)
-vim.keymap.set('n', '<F7>', ':Telescope fd<CR>', opts)
-vim.keymap.set('i', '<F7>', '<ESC>:Telescope fd<CR>', opts)
+require('telescope').setup{
+  defaults = {
+    file_ignore_patterns = { "node_modules" }
+  }
+}
+
+require("trouble").setup()
+vim.keymap.set("n", "<leader>xx", function() require("trouble").open() end)
+vim.keymap.set("n", "<leader>xw", function() require("trouble").open("workspace_diagnostics") end)
+vim.keymap.set("n", "<leader>xd", function() require("trouble").open("document_diagnostics") end)
+vim.keymap.set("n", "<leader>xq", function() require("trouble").open("quickfix") end)
+vim.keymap.set("n", "<leader>xl", function() require("trouble").open("loclist") end)
+vim.keymap.set("n", "gR", function() require("trouble").open("lsp_references") end)
 
 -- Setup for LuaLine
+
 require('lualine').setup()
 
 -- Setup for indent_blankline
 
 vim.opt.termguicolors = true
-
-vim.cmd [[highlight IndentBlanklineIndent1 guifg=#E06C75 gui=nocombine]]
-vim.cmd [[highlight IndentBlanklineIndent2 guifg=#E5C07B gui=nocombine]]
-vim.cmd [[highlight IndentBlanklineIndent3 guifg=#98C379 gui=nocombine]]
-vim.cmd [[highlight IndentBlanklineIndent4 guifg=#56B6C2 gui=nocombine]]
-vim.cmd [[highlight IndentBlanklineIndent5 guifg=#61AFEF gui=nocombine]]
-vim.cmd [[highlight IndentBlanklineIndent6 guifg=#C678DD gui=nocombine]]
-
 vim.opt.list = true
 
-require('indent_blankline').setup {
-  space_char_blankline = ' ',
-  char_highlight_list = {
-    "IndentBlanklineIndent1",
-    "IndentBlanklineIndent2",
-    "IndentBlanklineIndent3",
-    "IndentBlanklineIndent4",
-    "IndentBlanklineIndent5",
-    "IndentBlanklineIndent6",
-  },
+local highlight = {
+  "RainbowRed",
+  "RainbowYellow",
+  "RainbowBlue",
+  "RainbowOrange",
+  "RainbowGreen",
+  "RainbowViolet",
+  "RainbowCyan",
 }
+
+local hooks = require "ibl.hooks"
+-- create the highlight groups in the highlight setup hook, so they are reset
+-- every time the colorscheme changes
+hooks.register(hooks.type.HIGHLIGHT_SETUP, function()
+    vim.api.nvim_set_hl(0, "RainbowRed", { fg = "#E06C75" })
+    vim.api.nvim_set_hl(0, "RainbowYellow", { fg = "#E5C07B" })
+    vim.api.nvim_set_hl(0, "RainbowBlue", { fg = "#61AFEF" })
+    vim.api.nvim_set_hl(0, "RainbowOrange", { fg = "#D19A66" })
+    vim.api.nvim_set_hl(0, "RainbowGreen", { fg = "#98C379" })
+    vim.api.nvim_set_hl(0, "RainbowViolet", { fg = "#C678DD" })
+    vim.api.nvim_set_hl(0, "RainbowCyan", { fg = "#56B6C2" })
+end)
+
+require("ibl").setup { indent = { highlight = highlight } }
 
 -- Custom Commands
 
-vim.keymap.set('n', '<C-s>', ':w<CR>', opts)
+-- Save current file.
+vim.keymap.set('n', '<C-s>', '<CR>:w<CR>', opts)
 vim.keymap.set('i', '<C-s>', '<ESC>:w<CR>i', opts)
-vim.keymap.set('n', '<F8>', ':Autoformat<CR>', opts)
-vim.keymap.set('i', '<F8>', '<ESC>:Autoformat<CR>i', opts)
 
---- C/C++ Styling
-vim.api.nvim_create_autocmd('FileType c,cpp,h,hpp', {
-  command = 'silent! ClangFormatAutoEnable'
-})
+-- Format current file.
+vim.keymap.set('n', '<F4>', ':Autoformat<CR>', opts)
+vim.keymap.set('i', '<F4>', '<ESC>:Autoformat<CR>i', opts)
+
+-- Move lines
+vim.keymap.set('n', '<A-Down>', ':m .+1<CR>==', opts)
+vim.keymap.set('n', '<A-Up>', ':m .-2<CR>==', opts)
+vim.keymap.set('i', '<A-Down>', '<ESC>:m .+1<CR>==gi', opts)
+vim.keymap.set('i', '<A-Up>', '<ESC>:m .-2<CR>==gi', opts)
+vim.keymap.set('v', '<A-Down>', ':m \'>+1<CR>gv=gv', opts)
+vim.keymap.set('v', '<A-Up>', ':m \'<-2<CR>gv=gv',opts)
+
+-- Find Actions
+vim.keymap.set('n', '<C-F>', ':Telescope current_buffer_fuzzy_find<CR>', opts)
+vim.keymap.set('i', '<C-F>', '<ESC>:Telescope current_buffer_fuzzy_find<CR>', opts)
+vim.keymap.set('v', '<C-F>', ':Telescope current_buffer_fuzzy_find<CR>', opts)
+
+vim.keymap.set('n', '<F7>', ':Telescope live_grep<CR>', opts)
+vim.keymap.set('i', '<F7>', '<ESC>:Telescope live_grep<CR>', opts)
+vim.keymap.set('v', '<F7>', ':Telescope live_grep<CR>', opts)
+
+vim.keymap.set('n', '<F8>', ':Telescope fd<CR>', opts)
+vim.keymap.set('i', '<F8>', '<ESC>:Telescope fd<CR>', opts)
+vim.keymap.set('v', '<F8>', ':Telescope fd<CR>', opts)
+
+
